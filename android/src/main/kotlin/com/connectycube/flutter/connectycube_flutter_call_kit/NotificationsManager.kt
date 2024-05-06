aaf95d985a8537f2422251074a897f802d49c66e
Tai Pham <ptantai0401@gmail.com>
2024-05-06 16:43:29 +0700

    wrong file ringtone

--- android/src/main/kotlin/com/connectycube/flutter/connectycube_flutter_call_kit/NotificationsManager.kt
+++ android/src/main/kotlin/com/connectycube/flutter/connectycube_flutter_call_kit/NotificationsManager.kt
@@ -49,7 +49,7 @@ fun showCallNotification(
     var customRingtone = true
     Log.d("NotificationsManager", "customRingtone $customRingtone")
     if (customRingtone) {
-        ringtone = Uri.parse("android.resource://" + context.packageName + "/raw/ringtone")
+        ringtone = Uri.parse("android.resource://" + context.packageName + "/" + R.raw.ringtone)
         Log.d("NotificationsManager", "ringtone 1 $ringtone")
     } else {
         ringtone = Settings.System.DEFAULT_RINGTONE_URI
